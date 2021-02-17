7a965a24c978ec3956c5daf324b3d5e48814a397
Richard Felix <richard.felix@wemakecodes.com>
2021-02-17 17:19:15 -0300

    Realizando testes

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # macanix-chatbot
+
+## Treinar modelo
+rasa train
+
+## Executar com linha de comando
+rasa shell
+
+## Executar como serviço
+rasa run --m ./models --endpoints endpoints.yml --port 5005 -vv --enable-api --cors "*"
--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,29 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Any, Text, Dict, List, Union
 
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.forms import FormAction
 
-# This is a simple example for a custom action which utters "Hello World!"
+import requests
+import json
+import os
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+class ActionSubmitResults(Action):
+    def name(self) -> Text:
+        return "action_submit_results"
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict]:
+
+        # confirm_exercise = tracker.get_slot("confirm_exercise")
+        # exercise = tracker.get_slot("exercise")
+        # sleep = tracker.get_slot("sleep")
+        # stress = tracker.get_slot("stress")
+        # diet = tracker.get_slot("diet")
+        # goal = tracker.get_slot("goal")
+
+        dispatcher.utter_message("Achamos que seu carro tem o problema 📢!")
+        return []
