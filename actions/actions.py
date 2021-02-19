c3117d16601f8c1db2c8910178638c5a770e0c60
Richard Felix <richard.felix@wemakecodes.com>
2021-02-19 17:10:39 -0300

    Desenvolvido o chat de barulhos

--- actions/actions.py
+++ actions/actions.py
@@ -1,29 +1,29 @@
-from typing import Any, Text, Dict, List, Union
+# from typing import Any, Text, Dict, List, Union
 
-from rasa_sdk import Action, Tracker
-from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.forms import FormAction
+# from rasa_sdk import Action, Tracker
+# from rasa_sdk.executor import CollectingDispatcher
+# from rasa_sdk.forms import FormAction
 
-import requests
-import json
-import os
+# import requests
+# import json
+# import os
 
-class ActionSubmitResults(Action):
-    def name(self) -> Text:
-        return "action_submit_results"
-    def run(
-        self,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: Dict[Text, Any],
-    ) -> List[Dict]:
+# class ActionSubmitResults(Action):
+#     def name(self) -> Text:
+#         return "action_submit_results"
+#     def run(
+#         self,
+#         dispatcher: CollectingDispatcher,
+#         tracker: Tracker,
+#         domain: Dict[Text, Any],
+#     ) -> List[Dict]:
 
-        # confirm_exercise = tracker.get_slot("confirm_exercise")
-        # exercise = tracker.get_slot("exercise")
-        # sleep = tracker.get_slot("sleep")
-        # stress = tracker.get_slot("stress")
-        # diet = tracker.get_slot("diet")
-        # goal = tracker.get_slot("goal")
+#         confirm_exercise = tracker.get_slot("confirm_exercise")
+#         exercise = tracker.get_slot("exercise")
+#         sleep = tracker.get_slot("sleep")
+#         stress = tracker.get_slot("stress")
+#         diet = tracker.get_slot("diet")
+#         goal = tracker.get_slot("goal")
 
-        dispatcher.utter_message("Achamos que seu carro tem o problema 📢!")
-        return []
+#         dispatcher.utter_message("Achamos que seu carro tem o problema 📢!")
+#         return []
